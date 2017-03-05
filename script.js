64da7d8b4f20c52893d1ca6e10d6fbe1b8b9ef48
Ubuntu <ubuntu@ubuntu-xenial.localdomain>
2017-03-05 11:37:19 +0000

    Server -> static file server

--- script.js
+++ script.js
@@ -1,11 +1,21 @@
-var ps = require("ps-list");
+var http = require("http");
 var fs = require("fs");
+var url = require("url");
+var path  = require("path");
+var ps = require("ps-list");
 var procfs = require("procfs-stats");
 var process = require("process");
-var http = require("http");
-var port = process.env.PORT || 8001;
-var file = fs.readFileSync("index.html");
+var port = process.env.PORT || 8000;
 var io = require("socket.io").listen(server, {log: true});
+
+var file = fs.readFileSync("index.html");
+var mimeTypes = {
+    "html": "text/html",
+    "jpeg": "image/jpeg",
+    "jpg": "image/jpeg",
+    "png": "image/png",
+    "js": "text/javascript",
+    "css": "text/css"};
 
 var processList = [];
 ps().then(function(res){
@@ -27,7 +37,20 @@ function getStats(pids){
 
 var server = http.createServer(function(req, res){
     // Wow never use res.write without headers
-    res.setHeader('Content-Type', 'text/html');
-    console.log(file);
-    res.end(file);
+    var uri = url.parse(req.url).pathname
+    var filename = path.join(process.cwd(), uri);
+    console.log(filename);
+    fs.exists(filename, function(exists){
+	if(!exists){
+	    console.log("[404]" + filename);
+	    res.writeHead(200, {'Content-Type': 'text/plain'});
+	    res.write('404 Not Found\n');
+	    res.end();
+	}
+	var mimeType = mimeTypes[path.extname(filename).split(".")[1]];
+	res.writeHead(200, {'Content-Type': mimeType });
+	var fileStream = fs.createReadStream(filename);
+	fileStream.pipe(res);
+    });;
+
 }).listen(port);
